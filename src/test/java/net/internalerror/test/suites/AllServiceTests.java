f99416a0dce7daafb6c40e1d4c16ec9df47b4400
InternalErrorGit <phil.gatzka@gmail.com>
2024-01-12 22:44:07 +0100

    Fixing sonar issues

--- src/test/java/net/internalerror/test/suites/AllServiceTests.java
+++ src/test/java/net/internalerror/test/suites/AllServiceTests.java
@@ -1,9 +1,11 @@
 package net.internalerror.test.suites;
 
+import lombok.Generated;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
+@Generated
 @SelectPackages("net.internalerror.rest.service")
 public class AllServiceTests {
 }
--- src/test/java/net/internalerror/test/suites/AllTests.java
+++ src/test/java/net/internalerror/test/suites/AllTests.java
@@ -1,9 +1,11 @@
 package net.internalerror.test.suites;
 
+import lombok.Generated;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
+@Generated
 @SelectClasses({
         AllControllerTests.class,
         AllServiceTests.class,
