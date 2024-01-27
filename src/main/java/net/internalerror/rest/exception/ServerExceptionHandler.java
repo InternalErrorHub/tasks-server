4b6c1a5edb797261ae27727e7f0a44a1a3936b21
InternalErrorGit <phil.gatzka@gmail.com>
2024-01-27 22:44:06 +0100

    Fixed logging and exception

--- src/main/java/net/internalerror/rest/exception/ServerExceptionHandler.java
+++ src/main/java/net/internalerror/rest/exception/ServerExceptionHandler.java
@@ -1,8 +1,5 @@
 package net.internalerror.rest.exception;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -11,19 +8,28 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestControllerAdvice
 public class ServerExceptionHandler {
 
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Map<String, List<String>>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-    List<String> errors = e.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
-    return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
-  }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
 
-  private Map<String, List<String>> getErrorsMap(List<String> errors) {
-    Map<String, List<String>> errorResponse = new HashMap<>();
-    errorResponse.put("errors", errors);
-    return errorResponse;
-  }
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Map<String, List<String>>> validationException(ValidationException e) {
+        return new ResponseEntity<>(getErrorsMap(List.of(e.getMessage())), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
 
 }
